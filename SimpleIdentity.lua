1ddc31984c945407e3386e37874272c74ec19109
Travis Conway <travis@conway.us>
2026-08-15 09:36:28 -0700

    Fix copyright year to 2026 in SimpleIdentity.lua

--- SimpleIdentity.lua
+++ SimpleIdentity.lua
@@ -1,4 +1,4 @@
--- (c) 2006 Travis Conway
+-- (c) 2026 Travis Conway
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
